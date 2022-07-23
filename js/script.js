21171637117a0cd79a0d7c49626248c8421a9295
Deokma <dpopolamovmail@gmail.com>
2022-07-23 11:05:42 +0300

    main

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let photo = document.createElement("img");
+photo.src = "img/213.png";
+photo.width = "2955";
+photo.height = "2997";
+photo.alt = "map";
+
+let div1 = document.createElement('div');
+div1.append(photo);
+document.body.append(div1);
+/*var img = document.createElement("img");
+img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
+var src = document.getElementById("header");
+src.appendChild(img);*/
